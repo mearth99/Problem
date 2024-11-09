cf85d562f19578b2324c302c36f045d83dc75113
Lee jin ki <dlwlsrl99@gmail.com>
2024-11-09 14:22:57 +0900

    1109

--- README.py
+++ README.py
@@ -103,7 +103,7 @@ def create_select(categories):
     select = ""
     count = 0
     for category in categories:
-        if len(select + f"`{category}` ") < 30:
+        if len(select + f"`{category}` ") < 50:
             select += f"`{category}` "
         else:
             count += 1
