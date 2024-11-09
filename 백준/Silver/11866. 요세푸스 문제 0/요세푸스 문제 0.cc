c0da670fc169a92c029f8628732bd44dc43c7394
Lee Jin Ki <90100440+mearth99@users.noreply.github.com>
2024-11-09 13:44:47 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,43 @@
+/*
+    11866 오세푸스 문제0
+
+    요세푸스 문제는 다음과 같다.
+    1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 
+    이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
+    이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다.
+     예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+    N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
+/*
+    c언어는 배열을 통해서 큐를 구현하기 때문에 필연적으로 under , over flow문제가 생긴다. 이에 관련된 문제다.
+    N에 도달하기 전까지는 K 등차 수열 인덱스가 된다. 이때, N을 넘어가면 %N을 통해서 overflow를 해결한다. 만일 이미
+    배열을 탐색한 경우 contiue를 통해 다음 배열로 접근하게 해주면 된다.
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int que[1001]={0};
+    int N,K,index=0,count=0,end=0;
+    scanf("%d%d",&N,&K);
+    printf("<");
+    while(end!=N)
+    {
+        if(index>N)
+                index = index%N;
+        if(que[index]==0 && count==K)
+        {
+            que[index] = 1;
+            if(end!=N-1)
+                printf("%d, ",index);
+            else
+                printf("%d>",index);
+            count = 1;
+            end++;
+        }
+        else if(que[index]==0)
+            count++;
+        index++;
+    }
+    return 0;
+}
