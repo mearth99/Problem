2101be3074af6a0f5a14259586499d81bffdbdc5
Lee Jin Ki <90100440+mearth99@users.noreply.github.com>
2024-11-09 13:08:27 +0900

    [Gold II] Title: 빵집, Time: 80 ms, Memory: 21596 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int R, C, cnt = 0;
+int Map[10001][501];
+int dr[3] = {-1, 0, 1};
+int dc[3] = {1, 1, 1};
+ 
+void input(){
+    string in;
+    cin >> R >> C;
+    for(int i = 0; i < R; i++){
+        cin >> in;
+        for(int j = 0; j < C; j++){
+            if(in[j] == '.') Map[i][j] = 0;
+            else Map[i][j] = 1;
+        }
+    }
+}
+ 
+bool DFS(int r, int c){
+    if(c == C-1){
+        cnt++;
+        return true;
+    }
+    
+    Map[r][c] = 1;
+    
+    for(int i = 0; i < 3; i++){
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if(nr >= 0 && nr < R && nc >= 0 && nc < C && !Map[nr][nc]){
+            if(DFS(nr, nc)) return true;
+        }
+    }
+    
+    return false;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    input();
+
+// 0번 열에서 아래로 내려가면서 dfs실행
+    for(int i = 0; i < R; i++){
+        DFS(i, 0);
+    }
+    cout << cnt;
+    
+    return 0;
+}
+ 
