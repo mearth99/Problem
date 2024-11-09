527691d19957805fa17a0e5f02751850b3e32220
Lee Jin Ki <90100440+mearth99@users.noreply.github.com>
2024-11-09 14:01:57 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+	long long int a, b, tmp,c,d;
+	long long int result;
+	
+
+		scanf("%lld %lld", &a, &b);
+		c = a;
+		d = b;
+		while (b != 0)
+		{
+
+			tmp = b;
+			b = a % b;
+			a = tmp;
+		}
+		result = d * c / a;
+		printf("%lld\n%lld",a,result);
+		
+}
